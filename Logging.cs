e02ca1bf20b2fa4e8ce4d9b1f1c105231ab50b30
Yuki <Yuki.Walsh@Outlook.com>
2025-01-29 22:13:43 +1100

    initial

---
+++ Logging.cs
@@ -0,0 +1,91 @@
+﻿namespace StudioServer;
+
+using Serilog;
+using Serilog.Core;
+using Serilog.Events;
+using Serilog.Formatting;
+using System;
+using System.Diagnostics;
+using System.IO;
+
+public static class Logging
+{
+	public static readonly LoggerConfiguration Configuration;
+	private static readonly ILogger Logger;
+
+	static Logging()
+	{
+		Formatter formatter = new();
+
+		Configuration = new LoggerConfiguration();
+		Configuration.Enrich.With<StackEnricher>();
+		Configuration.WriteTo.Debug(formatter);
+		Configuration.WriteTo.Console(formatter);
+
+		Logger = Configuration.CreateLogger();
+	}
+
+	public static ILogger Shared => Logger;
+
+	public static ILogger ForContext<T>() => ForContext(typeof(T));
+	public static ILogger ForContext(Type type) => ForContext(type.Name);
+
+	public static ILogger ForContext(string context)
+	{
+		return Logger.ForContext("Context", context);
+	}
+
+	public static void Information(string message) => Shared.Information(message);
+}
+
+public class Formatter : ITextFormatter
+{
+	public void Format(LogEvent logEvent, TextWriter output)
+	{
+		output.Write("[");
+		output.Write(logEvent.Level);
+		output.Write("] ");
+
+		if (logEvent.Properties.TryGetValue("Context", out var contextValue))
+		{
+			output.Write("[");
+			if (contextValue is ScalarValue sv)
+			{
+				output.Write(sv.Value);
+			}
+
+			output.Write("] ");
+		}
+
+		output.Write(logEvent.MessageTemplate);
+
+		if (logEvent.Properties.TryGetValue("StackTrace", out var stackTrace))
+		{
+			output.WriteLine();
+			if (stackTrace is ScalarValue sv)
+			{
+				output.Write(sv.Value);
+			}
+		}
+
+		if (logEvent.Exception != null)
+		{
+			output.WriteLine();
+			output.WriteLine(logEvent.Exception.Message);
+			output.WriteLine(logEvent.Exception.StackTrace);
+		}
+
+		output.WriteLine();
+	}
+}
+
+public class StackEnricher : ILogEventEnricher
+{
+	public void Enrich(LogEvent logEvent, ILogEventPropertyFactory propertyFactory)
+	{
+		if (logEvent.Level >= LogEventLevel.Error && logEvent.Exception == null)
+		{
+			logEvent.AddOrUpdateProperty(propertyFactory.CreateProperty("StackTrace", new StackTrace(7, true)));
+		}
+	}
+}
---
+++ api/TestController.cs
@@ -0,0 +1,16 @@
+﻿namespace StudioServer.Api;
+
+using EmbedIO;
+using EmbedIO.Routing;
+using EmbedIO.WebApi;
+using System.Threading.Tasks;
+
+public class TestController : WebApiController
+{
+	[Route(HttpVerbs.Get, "/test")]
+	public Task PostData()
+	{
+		Swan.Logging.Logger.Info("Hello World!");
+		return Task.CompletedTask;
+	}
+}
