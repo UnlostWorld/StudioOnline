066f2c9e53cf2d8f03eb30d1c38d2aa417bcf159
Yuki <Yuki.Walsh@Outlook.com>
2025-02-24 19:31:33 +1100

    move submodule here

---
+++ StudioServer.Client/Analytics/AnalyticEvent.cs
@@ -0,0 +1,30 @@
+﻿namespace StudioServer.Client.Analytics;
+
+using System.Text.Json;
+
+public class AnalyticEvent
+{
+	public AnalyticEvents Event { get; set; }
+	public string? EventData { get; set; }
+
+	public Task Send()
+	{
+		string json = JsonSerializer.Serialize(this);
+		return StudioServerApi.PostAsync("/Analytics/Event", json, "application/json");
+	}
+
+	public static void Send(AnalyticEvents evt, string? data = null)
+	{
+		AnalyticEvent eventObj = new();
+		eventObj.Event = evt;
+		eventObj.EventData = data;
+		Task.Run(eventObj.Send);
+	}
+}
+
+public enum AnalyticEvents
+{
+	None = 0,
+
+	StudioStarted,
+}
