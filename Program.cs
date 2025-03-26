4aba03191aaf0f9c2143c324d9d579383cfee26c
Yuki <Yuki.Walsh@Outlook.com>
2025-03-26 23:25:17 +1100

    disable quartz until we have a real database

--- Program.cs
+++ Program.cs
@@ -43,14 +43,14 @@ public class Program
 
 		// OpenIddict offers native integration with Quartz.NET to perform scheduled tasks
 		// (like pruning orphaned authorizations/tokens from the database) at regular intervals.
-		services.AddQuartz(options =>
+		/*services.AddQuartz(options =>
 		{
 			options.UseSimpleTypeLoader();
 			options.UseInMemoryStore();
 		});
 
 		// Register the Quartz.NET service and configure it to block shutdown until jobs are complete.
-		services.AddQuartzHostedService(options => options.WaitForJobsToComplete = true);
+		services.AddQuartzHostedService(options => options.WaitForJobsToComplete = true);*/
 
 		services.AddDbContext<IddictContext>(options =>
 		{
