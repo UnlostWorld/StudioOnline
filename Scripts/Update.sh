d871c39f7c3eff53828b063d4b872e71d60754ef
Yuki <Yuki.Walsh@Outlook.com>
2025-03-26 23:04:50 +1100

    more update attempts

---
+++ Scripts/Update.sh
@@ -0,0 +1,10 @@
+#! /usr/bin/bash
+echo "Updating Studio Online..."
+echo "Stopping Server"
+pkill dotnet
+echo "Getting latest"
+git pull
+echo "building"
+dotnet publish --configuration Release
+echo "Starting Server"
+(dotnet ./bin/StudioOnline.dll &)
